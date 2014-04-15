da6335eed44c932ad95b95b6d5dc2cf073f36f06
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2014-04-15 18:42:07 +0200

    Web UI: change translation labels. Refs #2870

--- root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_FlashStart.php
+++ root/usr/share/nethesis/NethServer/Language/en/NethServer_Module_FlashStart.php
@@ -2,15 +2,15 @@
 
 /* NethServer_Module_FlashStart translation, language: en */
 
-$L['FlashStart_Description'] = 'FlashStart filters configuration';
-$L['FlashStart_Tags'] = 'FlashStart filter profile';
-$L['FlashStart_Title'] = 'FlashStart';
+$L['FlashStart_Description'] = 'Cloud filter configuration';
+$L['FlashStart_Tags'] = 'FlashStart cloud filter profile';
+$L['FlashStart_Title'] = 'Cloud content filter';
 $L['Group_label'] = 'Profile';
 $L['Key_label'] = 'Profile';
 $L['name_label'] = 'Name';
 $L['SrcProfile_Title'] = 'IP profiles';
 $L['UserProfile_Title'] = 'User profiles';
 $L['profile_create_header'] = 'Create new profile';
-$L['configure_header'] = 'Configure FlashStart profiles';
+$L['configure_header'] = 'Configure filter profiles';
 $L['external_page'] = 'By clicking below button, you will be redirect to an external site.';
-$L['flashstart_page_title'] = 'FlashStart profiles';
+$L['flashstart_page_title'] = 'FlashStart: edit profiles';
--- root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_FlashStart.php
+++ root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_FlashStart.php
@@ -2,15 +2,15 @@
 
 /* NethServer_Module_FlashStart translation, language: it */
 
-$L['FlashStart_Description'] = 'Configurazione filtri FlashStart';
-$L['FlashStart_Tags'] = 'FlashStart filtro profili profilo';
-$L['FlashStart_Title'] = 'FlashStart';
+$L['FlashStart_Description'] = 'Configurazione filtro contenuti Cloud';
+$L['FlashStart_Tags'] = 'FlashStart filtro profili profilo cloud';
+$L['FlashStart_Title'] = 'Filtro contenuti Cloud';
 $L['Group_label'] = 'Profilo';
 $L['Key_label'] = 'Profilo';
 $L['name_label'] = 'Nome';
 $L['SrcProfile_Title'] = 'Profili IP';
 $L['UserProfile_Title'] = 'Profili utente';
 $L['profile_create_header'] = 'Crea nuovo profilo';
-$L['configure_header'] = 'Configurazione profili FlashStart';
+$L['configure_header'] = 'Configurazione profili filtro contenuti';
 $L['external_page'] = 'Il pulsante sotto aprirà un sito esterno.';
-$L['flashstart_page_title'] = 'Profili FlashStart';
+$L['flashstart_page_title'] = 'FlashStart: modifica profili';
