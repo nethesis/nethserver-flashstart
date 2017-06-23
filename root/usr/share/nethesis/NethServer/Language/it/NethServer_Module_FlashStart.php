0990c28ba64a18006c2e169b1da552251a14e4fb
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2017-06-23 16:00:50 +0200

    Add Italian translation

---
+++ root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_FlashStart.php
@@ -0,0 +1,23 @@
+<?php 
+
+/* NethServer_Module_FlashStart translation, language: it */
+
+$L['FlashStart_Description'] = 'Configurazione filtro cloud';
+$L['FlashStart_Tags'] = 'FlashStart cloud filter profile filtro profilo';
+$L['FlashStart_Title'] = 'Filtro contenuti cloud';
+$L['FlashStartReport_Description'] = 'Report filtro contenuti cloud';
+$L['FlashStartReport_Tags'] = 'Report filtro contenuti cloud FlashStart';
+$L['FlashStartReport_Title'] = 'Filtro contenuti cloud';
+$L['FlashStart_header'] = 'Filtro contenuti cloud FlashStart';
+$L['Flashstart_Registration'] = 'Prima di usare FlashStart, è necessario creare un account dal sito riportato sotto.';
+$L['FlashStartSite_label'] = 'Registra e configura il filtro';
+$L['Status_disabled_label'] = 'Filtro disabilitato';
+$L['Status_enabled_label'] = 'Filtro abilitato';
+$L['Username_label'] = 'Nome utente (mail)';
+$L['Password_label'] = 'Password';
+$L['Roles_label'] = 'Filtra le reti seguenti:';
+$L['green_label'] = 'Reti locali (interfacce green)';
+$L['blue_label'] = 'Reti ospiti (interfacce blue)';
+$L['Bypass_label'] = 'Bypass IP sorgente';
+$L['valid_bypass'] = '"${0}" non è un IP valido';
+$L['bad_login'] = 'Nome utente e/o password non validi.';
